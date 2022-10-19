e2eb6aa56401f91e218bd3cd35e4ebe362716c95
Harishanaga <nagaharisha99@gmail.com>
2022-10-19 16:16:55 +0530

    harisha commit

---
+++ SDET_9_HYD/src/test/java/harisha/program1.java
@@ -0,0 +1,11 @@
+package harisha;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;int b=10;
+		System.out.println("hai harisha");
+	}
+
+}
