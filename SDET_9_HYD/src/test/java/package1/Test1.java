b732b84375754ebde92c77761f21b1bc11baee8e
91995 <91995@Tiger>
2022-10-19 11:03:16 +0530

    changed from unstaged to staged area

---
+++ SDET_9_HYD/src/test/java/package1/Test1.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;int b=30;
+		System.out.println(a+b);
+	}
+
+}
