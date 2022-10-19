2588738cb9a124c5e9ee8d1e26bc3fef8ce44f8c
91995 <91995@Tiger>
2022-10-19 11:23:03 +0530

    changed values

--- SDET_9_HYD/src/test/java/package1/Test1.java
+++ SDET_9_HYD/src/test/java/package1/Test1.java
@@ -4,8 +4,10 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=20;int b=30;
-		System.out.println(a+b);
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println(c);
 	}
 
 }
