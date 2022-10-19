d91288085f66ebc7aea52974f4062294e6700a13
91995 <91995@Tiger>
2022-10-19 11:34:55 +0530

    javapractice unstsged to staged

---
+++ JavaPractice/src/programs/Sample1.java
@@ -0,0 +1,23 @@
+package programs;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []a= {10,2,3,4,10,4};
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]==a[j])
+				{
+					System.out.println(a[j]);
+					break;
+				}
+			}
+		}
+		
+		
+	}
+
+}
